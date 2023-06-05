9a83fdee1b90167f95e510731c6829a08b53230c
creativehabib <shamolrahaman@gmail.com>
2023-06-06 01:40:20 +0600

    Vue Router add

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 10 , Vue.js 3 , Tailwind CSS and Vite Project</title>
+      @vite('resources/css/app.css')
+</head>
+<body>
+  <div id="app"></div>
+  @vite('resources/js/app.js')
+</body>
+</html>
